f0fed219e4bd7c4944f49baa5feae5a2aff92dc7
Anuj Pawar <anujpawar13@gmail.com>
2021-11-26 21:42:43 +0530

    routes fixed

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const PORT = 5000;
 app.set("view engine", "ejs");
 
 app.get('/', (req, res)=>{
-    res.render("index")
+    res.render("index", {active: "dashboard"})
+});
+
+app.get('/instagram', (req, res)=>{
+    res.render("instagram", {active: "analytics"} )
+});
+
+app.get('/twitter', (req, res)=>{
+    res.render("twitter")
 });
 
 
